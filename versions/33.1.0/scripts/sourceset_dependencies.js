e418e4b6f8e2f4e413f519103fec107ae0cc2f9b
marianeum <marianeum@users.noreply.github.com>
2021-11-04 18:12:01 +0000

    deploy: Skyscanner/backpack-android@2f61ef75b0dd36e22a4a37b1ebab4bcc02241348

---
+++ versions/33.1.0/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":Backpack:generateDokkaDocs/androidTestRelease":[],":Backpack:generateDokkaDocs/debug":[],":Backpack:generateDokkaDocs/main":[],":Backpack:generateDokkaDocs/release":[]}'
